3073eb933b48d3d11d07e2cf1e27040621010f78
Peter Boling <peter.boling@gmail.com>
2015-10-21 20:27:04 -0700

    Bare

---
+++ lib/rspec/pending_for.rb
@@ -0,0 +1,7 @@
+require "rspec/pending_for/version"
+
+module Rspec
+  module PendingFor
+    # Your code goes here...
+  end
+end
---
+++ spec/rspec/pending_for_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Rspec::PendingFor do
+  it 'has a version number' do
+    expect(Rspec::PendingFor::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
