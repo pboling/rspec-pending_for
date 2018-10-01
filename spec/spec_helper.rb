9427f018b0f7e84b283b08556aac17196712e935
Peter Boling <peter.boling@gmail.com>
2018-10-01 03:13:04 -0700

    Have to use __FILE__ until support for Ruby < 2 is dropped

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,3 @@
-$LOAD_PATH.unshift File.expand_path('../lib', __dir__)
+# NOTE: Have to use __FILE__ until Ruby 1.x support is dropped
+$LOAD_PATH.unshift File.expand_path('../lib', __FILE__)
 require 'rspec/pending_for'
